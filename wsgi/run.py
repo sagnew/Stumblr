92129d376cbac830dc2928f3a5b69814ac1d69a6
Jesse Huang <jh3141@gmail.com>
2013-01-19 13:05:16 -0500

    modified run.py

--- wsgi/run.py
+++ wsgi/run.py
@@ -1,15 +1,26 @@
-import os
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect, url_for
+
+import os
 
 app = Flask(__name__)
 
-#Create our index or root / route
-@app.route("/")
-@app.route("/index")
-def index():
-    return "This is the application mynote & I'm alive"
+@app.route('/')
+def main_page():
+	return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug = "True")
+@app.route('/stumbl', methods=['POST', 'GET'])
+def stumbl():
+	return render_template('index.html')
+
+@app.route('/test', methods=['POST', 'GET'])
+def test():
+	return render_template('test.html')
+
+
+if __name__ == '__main__':
+	port = int(os.environ.get("PORT",5000))
+	app.run(host='0.0.0.0', port=port)
+
