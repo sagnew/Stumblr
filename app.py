0663323ae0936e6eddee060378a4bf0731e6a3cb
Sam Agnew <sam@sam-Studio-1555.(none)>
2013-01-19 15:47:16 -0500

    Having some encoding errors

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect, url_for
-
+import backend
 import os
 
 app = Flask(__name__)
@@ -14,18 +14,17 @@ def main_page():
 @app.route('/stumbl', methods=['POST', 'GET'])
 def stumbl():
     with open('templates/stumbl.html', 'w') as page:
-        with open('index.html') as original:
+        with open('templates/index.html', 'r') as original:
             #Do string shit to replace iframe url
-            replacement = original.read().replace("", "")
-
+            replacement = original.read()
+            replacement = replacement.replace("http://tumblr.com", backend.getNextUrl())
+            page.write(replacement)
 	return render_template('stumbl.html')
 
 @app.route('/test', methods=['POST', 'GET'])
 def test():
 	return render_template('test.html')
 
-
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT",5000))
-	app.run(host='0.0.0.0', port=port)
-
+	app.run(host='0.0.0.0', port=port, debug=True)
