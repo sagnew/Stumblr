e4a4e7bc14cfe6f79725424210daf7dd0347d056
Jesse Huang <jh3141@gmail.com>
2013-01-19 13:38:46 -0500

    adding template folders and files

--- wsgi/run.py
+++ wsgi/run.py
@@ -7,9 +7,23 @@ app = Flask(__name__)
 
 #Create our index or root / route
 @app.route("/")
-@app.route("/index")
+@app.route("/index", methods=["GET", "POST"])
 def index():
-    return "This is the application mynote & I'm alive"
+    notes = [{ "name": "First Note Ever",
+              "author":"Angel",
+              "content":"This text is coming from the content field"
+               },
+             { "name":"Finish this blog",
+              "author":"Angel",
+              "content":"Show the template control structures"
+               },
+             { "name":"Deploy this shit",
+              "author":"Angel",
+              "content":"When finished, this is the stuff"
+               },
+
+             ]
+    return render_template("index.html", notes = notes)
 
 if __name__ == "__main__":
     app.run(debug = "True")
