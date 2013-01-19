b19a195c1e4ebf8fc1eccc2921716cf5979f9317
Sam Agnew <sam@sam-Studio-1555.(none)>
2013-01-19 04:47:20 -0500

    Minor change

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def main_page():
 	return render_template('index.html')
 
+@app.route('/stumbl', methods=['POST', 'GET'])
+def stumbl():
+    return render_template('index.html')
+
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT",5000))
 	app.run(host='0.0.0.0', port=port)
