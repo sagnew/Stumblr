49db955c590901c2bdc459797e96635153ace1a7
Sam Agnew <sam@sam-Studio-1555.(none)>
2013-01-19 04:00:54 -0500

    Finally throwing the app and html up there!

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def main_page():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	port = int(os.environ.get("PORT",5000))
+	app.run(host='0.0.0.0', port=port)
+
