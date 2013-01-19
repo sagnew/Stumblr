019f4dc437a0e2ef89c63ed616075764a62f37ce
Jesse Huang <jh3141@gmail.com>
2013-01-19 14:26:58 -0500

    testing

---
+++ wsgi/static/js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    /*$(document).on('focus', '#text_input', function(){
+
+        if($(this).val() == $(this)[0].defaultValue){
+            $(this).val('');
+        }
+
+    });
+
+    $(document).on('blur', '#text_input', function(){
+
+        if($(this).val() === ''){
+            $(this).val( $(this)[0].defaultValue );
+        }
+
+    });*/
+
+    $('#like').click( function(){
+        //Do stuff here
+        alert('Placeholder');
+    });
+
+    $('#dislike').click( function(){
+        //Do stuff here
+        alert('Placeholder');
+    });
+
+    $('#favorites').click( function(){
+        //Do stuff here
+        alert('Placeholder');
+    });
+
+});
