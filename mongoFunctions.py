438abcc24f7c74c91198ec260cf4d86efdf58ca9
Sam Agnew <sam@sam-Studio-1555.(none)>
2013-01-20 06:28:03 -0500

    Fixed Ruell's mongo functions

--- mongoFunctions.py
+++ mongoFunctions.py
@@ -1,50 +1,67 @@
 import pymongo
 
 connection = pymongo.Connection()
-db = connection.Stumblr
+db = connection.Stumblr.posts
 
-def insert_user(userID):
-	#Inserts a user's informat into the collection.
-	post = {"userID": userID,
-			"tags": {},
-			"urls": {}
-			}
-	#only insert if the user is not already in the collection.
-	if db.post.find({"userID": userID}).count() == 0:
-		#collection.insert(post)
-		print userID
+def insert_user(userID, tags):
+    #Inserts a user's informat into the collection.
+    post = {"userid": userID,
+    		"tags": tags,
+    		"urls": {}
+    		}
+    #only insert if the user is not already in the collection.
+    if db.find({"userid": userID}).count() == 0:
+    	db.insert(post)
 
 def add_tags(userID, tags):
-	val = db.posts.find_one({'userid': userID})
-	tagVals = val['tags']
+    val = db.find_one({'userid': userID})
+    tagVals = val['tags']
 
-	for t in tags:
-		if t not in tagVals.keys():
-			tagVals[t] = 0
-
+    for t in tags:
+    	if t not in tagVals.keys():
+        	tagVals[t] = 0
+    db.update({'userid': userID}, tagVals)
 
 def increment_tags(userID, tags):
-	val = db.posts.find_one({'userid': userID})
-	tagVals = val['tags']
+    val = db.find_one({'userid': userID})
+    tagVals = val['tags']
 
-	incList = []
+    incList = []
 
-	for t in tags:
-		if t in tagVals.keys():
-			tagVals[t] = tagVals[t] + 1
-		else:
-			incList.append(t)
-	db.posts.update({'userid':userID},val)
+    for t in tags:
+    	if t in tagVals.keys():
+    	    tagVals[t] = tagVals[t] + 1
+    	else:
+    		incList.append(t)
+    db.update({'userid':userID},val)
 
 def decrement_tags(userID, tags):
-	val = db.posts.find_one({'userid': userID})
-	tagVals = val['tags']
+    val = db.find_one({'userid': userID})
+    tagVals = val['tags']
 
-	decList = []
+    decList = []
 
-	for t in tags:
-		if t in tagVals.keys():
-			tagVals[t] = tagVals[t] - 1
-		else:
-			decList.append(t)
-	db.posts.update({'userid':userID},val)
+    for t in tags:
+    	if t in tagVals.keys():
+    		tagVals[t] = tagVals[t] - 1
+    	else:
+    		decList.append(t)
+    db.update({'userid':userID},val)
+
+db.drop()
+
+insert_user(25, {"Kung Fu": 1, "Hacking": 1})
+for item in db.find():
+    print item
+print "  "
+increment_tags(25, ["Kung Fu", "Hacking"])
+for item in db.find():
+    print item
+print " "
+decrement_tags(25, ["Kung Fu", "Hacking"])
+for item in db.find():
+    print item
+print " "
+add_tags(25, ["Fighting", "Guitar"])
+for item in db.find():
+    print item
