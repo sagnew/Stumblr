d76836f12b2e0413c7a53b251e41d6eb1d3e9dd4
Jesse Huang <jh3141@gmail.com>
2013-01-19 20:52:55 -0500

    removed some mongo stuff

--- wsgi/run.py
+++ wsgi/run.py
@@ -6,11 +6,11 @@ import pymongo
 import backend
 
 data_dir = os.environ['OPENSHIFT_DATA_DIR']
-mongo_con = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_HOST'],
+#mongo_con = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_HOST'],
                                int(os.environ['OPENSHIFT_MONGODB_DB_PORT']))
 
-mongo_db = mongo_con[os.environ['OPENSHIFT_APP_NAME']]
-mongo_db.authenticate(os.environ['OPENSHIFT_MONGODB_DB_USERNAME'],
+#mongo_db = mongo_con[os.environ['OPENSHIFT_APP_NAME']]
+#mongo_db.authenticate(os.environ['OPENSHIFT_MONGODB_DB_USERNAME'],
                       os.environ['OPENSHIFT_MONGODB_DB_PASSWORD'])
 
 
@@ -61,49 +61,49 @@ def tutorial():
 
 # Mongo DB functions
 
-def user_find(userid):
-    if not userid: return None
-    return mongo_db.users.find_one({ '_id': userid})
+# def user_find(userid):
+#     if not userid: return None
+#     return mongo_db.users.find_one({ '_id': userid})
 
-def user_create(username, password):
-    if not username: return None
-    # check for pre existance
-    tuser = user_find(username)
-    if tuser: return None
+# def user_create(username, password):
+#     if not username: return None
+#     # check for pre existance
+#     tuser = user_find(username)
+#     if tuser: return None
 
-    nuser = {
-        '_id': username,
-        'pw': password,
-        'follower': [ ],
-        'followee': [ ],
-        'timeline': [ ],
-        'posts': [ ],
-        }
-    userid = mongo_db.users.insert(nuser)
-    return userid
+#     nuser = {
+#         '_id': username,
+#         'pw': password,
+#         'follower': [ ],
+#         'followee': [ ],
+#         'timeline': [ ],
+#         'posts': [ ],
+#         }
+#     userid = mongo_db.users.insert(nuser)
+#     return userid
 
-def user_list():
-    l = []
-    for u in mongo_db.users.find():
-        l.append(u['_id'])
-    l.sort()
-    return l
+# def user_list():
+#     l = []
+#     for u in mongo_db.users.find():
+#         l.append(u['_id'])
+#     l.sort()
+#     return l
 
-def user_follow(user, tuser):
-    user['followee'].append(tuser['_id'])
-    mongo_db.users.update({ '_id': user['_id']}, user)
-    tuser['follower'].append(user['_id'])
-    mongo_db.users.update({ '_id': tuser['_id']}, tuser)
-    return
+# def user_follow(user, tuser):
+#     user['followee'].append(tuser['_id'])
+#     mongo_db.users.update({ '_id': user['_id']}, user)
+#     tuser['follower'].append(user['_id'])
+#     mongo_db.users.update({ '_id': tuser['_id']}, tuser)
+#     return
 
-def user_unfollow(user, tuser):
-    if tuser['_id'] in user['followee']:
-        user['followee'].remove(tuser['_id'])
-    mongo_db.users.update({ '_id': user['_id']}, user)
-    if user['_id'] in tuser['follower']:
-        tuser['follower'].remove(user['_id'])
-    mongo_db.users.update({ '_id': tuser['_id']}, tuser)
-    return
+# def user_unfollow(user, tuser):
+#     if tuser['_id'] in user['followee']:
+#         user['followee'].remove(tuser['_id'])
+#     mongo_db.users.update({ '_id': user['_id']}, user)
+#     if user['_id'] in tuser['follower']:
+#         tuser['follower'].remove(user['_id'])
+#     mongo_db.users.update({ '_id': tuser['_id']}, tuser)
+#     return
 
 
 
