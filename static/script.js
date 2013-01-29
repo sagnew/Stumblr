8336a66b4334b7b4772379e68ec767319ed1aa3a
Sam Agnew <sam@sam-Studio-1555.(none)>
2013-01-28 19:44:58 -0500

    Adding view favorites

--- static/script.js
+++ static/script.js
@@ -1,19 +1,33 @@
 $(document).ready(function(){
 
+    var hasBeenClicked = false;
+
     $('#prompt').hide();
     $('#frame').hide();
+    $('#list').hide();
+
     if($('#user').val() == ''){
         $('#prompt').slideDown('slow');
+        $('group').hide();
     }else{
         $('#frame').fadeIn('slow');
     }
 
+    $('#view').on("click", function(){
+        if(hasBeenClicked){
+            $('#list').slideUp('slow');
+            $('#frame').slideDown('slow');
+            hasBeenClicked = false;
+        }else{
+            $('#frame').slideUp('slow');
+            $('#list').slideDown('slow');
+            hasBeenClicked = true;
+        }
+    });
+
 });
 
-function submitForm()
-{
+function submitForm(){
     document.form.action = document.pressed;
-    data.url = $('#tumblr').val();
-    $.post(document.pressed, data);
     return true;
 }
