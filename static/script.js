7ed402ea075dfa0d9d9a92751238ef1c3955f303
Sam Agnew <sam@sam-Studio-1555.(none)>
2013-02-02 16:25:16 -0500

    Added jQuery variables

--- static/script.js
+++ static/script.js
@@ -1,35 +1,46 @@
 $(document).ready(function(){
 
-    $('#prompt').hide();
-    $('#frame').hide();
-    $('#list').hide();
-    $('#add_to_favorites').hide();
+    $userPrompt = $('#prompt');
+    $frame = $('#frame');
+    $list = $('#list');
+    $add_to_favorites = $('#add_to_favorites');
+    $group = $('#group');
+    $user = $('#user');
+    $favorites = $('#favorites');
+    $fav_icon = $('.fav_icon');
 
-    if($('#user').val() == ''){
-        $('#prompt').slideDown('slow');
-        $('#group').fadeOut('slow');
+    $userPrompt.hide();
+    $frame.hide();
+    $list.hide();
+    $add_to_favorites.hide();
+
+    if($user.val() == ''){
+        $userPrompt.slideDown('slow');
+        $group.fadeOut('slow');
     }else{
-        $('#frame').fadeIn('slow');
-        $('#group').fadeIn('slow');
+        $frame.fadeIn('slow');
+        $group.fadeIn('slow');
     }
 
-    $('#favorites').click(function(){
-        $('#list').fadeOut('slow');
-        $('#frame').slideToggle('slow');
-        $('#add_to_favorites').slideToggle('slow');
+    $favorites.click(function(){
+        $list.fadeOut('slow');
+        $frame.slideToggle('slow');
+        $add_to_favorites.slideToggle('slow');
     });
 
-    $('#view').click(function(){
-        $('#add_to_favorites').fadeOut('slow');
-        $('#list').slideToggle('slow');
-        $('#frame').slideToggle('slow');
+    $view.click(function(){
+        $add_to_favorites.fadeOut('slow');
+        $list.slideToggle('slow');
+        $frame.slideToggle('slow');
     });
 
-    $('.fav_icon').click(function(){
-        $(this).preventDefault();
-        $('#frame').attr("src", $(this).attr("href"));
-        $('#list').slideUp('slow');
-        $('#frame').slideDown('slow');
+    $fav_icon.click(function(){
+        $thisIcon = $(this);
+
+        $thisIcon.preventDefault();
+        $frame.attr("src", $thisIcon.attr("href"));
+        $list.slideUp('slow');
+        $frame.slideDown('slow');
     });
 
 });
